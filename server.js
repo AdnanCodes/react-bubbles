d248f9eb38aaa921be1b517b3d9865917d2005d7
Adnan <adnan.chowdhury@outlook.com>
2019-08-23 10:52:19 -0500

    Added PrivateRoute to BubblePage, AxioswithAuth for Login

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ function authenticator(req, res, next) {
 
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
-  if (username === "lambda" && password === "lambda") {
+  if (username === "lambda" && password === "test") {
     req.loggedIn = true;
     setTimeout(() => {
       res.status(200).json({
